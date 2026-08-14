ff23cec65365804567291488d59e64d2581a872e
hyuxxi1 <hyunjee5783@naver.com>
2026-08-14 11:50:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.set_page_config(
 if "NEIS_API_KEY" in st.secrets:
     NEIS_API_KEY = st.secrets["NEIS_API_KEY"]
 else:
-    NEIS_API_KEY = "YOUR_NEIS_API_KEY_HERE"  # 여기에 본인 API 키 입력
+    NEIS_API_KEY = "475158beb13640a08d94b5fa99bb678f"  # 여기에 본인 API 키 입력
 
 
 def get_school_info(api_key, school_name):
